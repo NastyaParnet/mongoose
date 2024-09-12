57f5dc4005668ecfe97e7c6f541ce833d6dcb7ac
NastyaParnet <nastya.parnet.01@gmail.com>
2024-09-12 19:56:33 +0300

    add postArticle

--- controllers/articleController.js
+++ controllers/articleController.js
@@ -4,7 +4,25 @@ exports.getAllArticles = async () => {};
 
 exports.getArticle = async () => {};
 
-exports.postArticle = async () => {};
+exports.postArticle = async (req, res) => {
+  try {
+    const article = new Article(req.body);
+    const result = await article.save();
+    res.status(201).json({
+      status: 'success',
+      data: {
+        article: result,
+      },
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: 'error',
+      message: {
+        error,
+      },
+    });
+  }
+};
 
 exports.patchArticle = async () => {};
 
